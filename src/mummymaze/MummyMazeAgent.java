af9cc1e5ab5537f85e96da8e61647a4667f396cc
Ana Martins <anaritamsmartins@gmail.com>
2022-04-10 10:13:50 +0100

    MMAgent String to Matrix and MMState Exit's line and column

--- src/mummymaze/MummyMazeAgent.java
+++ src/mummymaze/MummyMazeAgent.java
@@ -21,14 +21,27 @@ public class MummyMazeAgent extends Agent<MummyMazeState> {
     public MummyMazeState readInitialStateFromFile(File file) throws IOException {
         java.util.Scanner scanner = new java.util.Scanner(file);
 
-        char[][] matrix = new char [13][13];
-
+        /*char[][] matrix = new char [13][13];
         for (int i = 0; i < 13; i++) {
             for (int j = 0; j < 13; j++) {
                 matrix[i][j] = (char) scanner.nextByte();
             }
             scanner.nextLine();
+        }*/
+
+        // String -> Matriz
+        int i=0, j=0;
+        char matrix[][] = new char[13][13];
+        for (char t:  scanner.nextLine().toCharArray()){
+            if(t!='\n') {
+                matrix[i][j] = t;
+                j++;
+            }else{
+                j=0;
+                i++;
+            }
         }
+
         initialEnvironment = new MummyMazeState(matrix);
         resetEnvironment();
         return environment;
