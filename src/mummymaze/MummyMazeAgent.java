8bd0763f8531b250b35a4c7f9f0dbf4445988d38
André Pinto <andresantospinto26@gmail.com>
2022-04-12 10:13:48 +0100

    configured readInitialStateFromFile in MMAgent

--- src/mummymaze/MummyMazeAgent.java
+++ src/mummymaze/MummyMazeAgent.java
@@ -3,6 +3,8 @@ import agent.Agent;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MummyMazeAgent extends Agent<MummyMazeState> {
 
@@ -19,16 +21,32 @@ public class MummyMazeAgent extends Agent<MummyMazeState> {
     }
 
     public MummyMazeState readInitialStateFromFile(File file) throws IOException {
-        java.util.Scanner scanner = new java.util.Scanner(file);
+        java.util.Scanner scanner = new java.util.Scanner(file).useDelimiter("\r\n");
+        List<String> stringList = new ArrayList<>();
+        String str;
+        while (scanner.hasNext()){
+            str = scanner.nextLine();
+            stringList.add(str);
+        }
+
+        char[][] matrix = new char [13][13];
+        int i = 0;
+        for (String s: stringList) {
+            for (int k = 0; k < s.length(); k++) {
+                matrix[i][k] = s.toCharArray()[k];
+            }
+            i++;
+        }
+ /*java.util.Scanner scanner = new java.util.Scanner(file);
 
         char[][] matrix = new char [13][13];
         for (int i = 0; i < 13; i++) {
             for (int j = 0; j < 13; j++) {
-                matrix[i][j] = (char) scanner.nextByte();
+                    matrix[i][j] = (char) scanner.nextByte();
             }
             scanner.nextLine();
         }
-
+*/
         // String -> Matriz
         /*
         int i=0, j=0;
